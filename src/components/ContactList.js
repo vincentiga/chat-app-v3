9ecbaa8b2f1c32dc3232ec3d18dc26665083f5db
Vincent <igabillevincent@outlook.fr>
2020-04-08 22:27:09 +0200

    ContactList s update

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -5,36 +5,36 @@ const users = [
       id: "1",
       name: 'Robert Reyes',
       avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
-      online: "online",
-      on: "status-online"
+      online: true,
+      on: true
     },
     {
       id: "2",
       name: 'Nellie Caldwell',
       avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
-      online: "online",
-      on: "status-online"
+      online: true,
+      on: true
     },
     {
       id: "3",
       name: 'Vernon Mason',
       avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
-      online: "online",
-      on: "status-online"
+      online: true,
+      on: true
     },
     {
       id: "4",
       name: 'Erica Hunt',
       avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
-      online: "offline",
-      on: ""
+      online: false,
+      on: false
     },
     {
       id: "5",
       name: 'Juanita Phillips',
       avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
-      online: "online",
-      on: "status-online"
+      online: true,
+      on: true
     }
   ];
 
