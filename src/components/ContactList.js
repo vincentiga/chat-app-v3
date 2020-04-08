eb62345a9fa9bbfdcdf94b0749aa3359d9555453
Vincent <igabillevincent@outlook.fr>
2020-04-08 02:11:02 +0200

    update ContactList

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -5,38 +5,43 @@ const users = [
       id: "1",
       name: 'Robert Reyes',
       avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
-      online: false
+      online: "online",
+      on: "status-online"
     },
     {
       id: "2",
       name: 'Nellie Caldwell',
       avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
-      online: true
+      online: "online",
+      on: "status-online"
     },
     {
       id: "3",
       name: 'Vernon Mason',
       avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
-      online: true
+      online: "online",
+      on: "status-online"
     },
     {
       id: "4",
       name: 'Erica Hunt',
       avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
-      online: false
+      online: "offline",
+      on: ""
     },
     {
       id: "5",
       name: 'Juanita Phillips',
       avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
-      online: true
+      online: "online",
+      on: "status-online"
     }
   ];
 
   const ContactList = () => (
     <div>
       {users.map(item => (
-      <Contact name={item.name} avatar={item.avatar} online={item.online} key={item.id} />
+      <Contact name={item.name} avatar={item.avatar} online={item.online} on={item.on} key={item.id} />
       ))}
     </div>
   );
